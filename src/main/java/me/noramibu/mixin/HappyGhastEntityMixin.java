f99b606c22aeae6e8fe2e3d81dd4a937ce89edde
noramibuu <ugurum5552@gmail.com>
2025-07-23 22:52:44 +0300

    fix collision

--- src/main/java/me/noramibu/mixin/HappyGhastEntityMixin.java
+++ src/main/java/me/noramibu/mixin/HappyGhastEntityMixin.java
@@ -1,18 +1,21 @@
 package me.noramibu.mixin;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.passive.HappyGhastEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.vehicle.ChestMinecartEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.Team;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.entity.EquipmentSlot;
 
 @Mixin(HappyGhastEntity.class)
 public abstract class HappyGhastEntityMixin {
@@ -20,7 +23,6 @@ public abstract class HappyGhastEntityMixin {
     private void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         HappyGhastEntity ghast = (HappyGhastEntity) (Object) this;
         ItemStack itemStack = player.getStackInHand(hand);
-        // Only spawn if the player is holding a CHEST_MINECART
         if (itemStack.isOf(Items.CHEST_MINECART) && ghast.getPassengerList().size() < 3 && !ghast.getEquippedStack(EquipmentSlot.BODY).isEmpty()) {
             if (!ghast.getWorld().isClient) {
                 ChestMinecartEntity chestMinecart = new ChestMinecartEntity(EntityType.CHEST_MINECART, ghast.getWorld());
@@ -29,6 +31,22 @@ public abstract class HappyGhastEntityMixin {
                 chestMinecart.startRiding(ghast);
                 if (!player.getAbilities().creativeMode)
                     itemStack.decrement(1);
+                if (ghast.getWorld() instanceof ServerWorld serverWorld) {
+                    Scoreboard scoreboard = serverWorld.getScoreboard();
+                    Team team = scoreboard.getTeam("NoCollision");
+                    if (team == null) {
+                        team = scoreboard.addTeam("NoCollision");
+                        team.setCollisionRule(Team.CollisionRule.NEVER);
+                    } else if (team.getCollisionRule() != Team.CollisionRule.NEVER) {
+                        team.setCollisionRule(Team.CollisionRule.NEVER);
+                    }
+                    String command = String.format("team join NoCollision %s", chestMinecart.getUuidAsString());
+                    try {
+                        serverWorld.getServer().getCommandManager().executeWithPrefix(
+                            serverWorld.getServer().getCommandSource().withSilent(), command
+                        );
+                    } catch (Exception ignored) {}
+                }
             }
             cir.setReturnValue(ActionResult.SUCCESS);
         }
